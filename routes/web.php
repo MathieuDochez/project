1b8c994418e7e5408c554ebf40d41d20545d86a4
MathieuDochez <r0893166@student.thomasmore.be>
2024-10-22 17:09:09 +0200

    added layout not working

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Livewire\Home;use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class);
 
 Route::middleware([
     'auth:sanctum',
